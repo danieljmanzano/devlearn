4952917f02fd7b12e9352d3eebad926a7fa41168
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2025-05-14 21:15:01 -0300

    vou aprender tudo que der ah muleke

---
+++ primeiro/main.js
@@ -0,0 +1,6 @@
+console.log("nao sei o que nao sei o que la");
+
+var elementos = document.getElementById("testando");
+console.log(elementos);
+
+document.getElementById("mensagem").textContent = "sabonete";
